e3ca3f88b04680a2c7e7c2c2ab4f72c7780a1928
Steven Lay <arbiter1000@gmail.com>
2021-11-18 23:48:47 +1100

    Add initial and finals tables

---
+++ src/components/PhonoTable.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/no-danger */
+import styled from 'styled-components'
+
+function numToSuperScript(inputStr) {
+  return inputStr.replace(/([0-9])/g, '<sup>$&</sup>')
+}
+
+/* eslint-disable camelcase */
+export default function PhonoTable({ data, colTitles }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {colTitles.map(title => (
+            <th key={title}>{title}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(item => (
+          <tr key={item.entry}>
+            <td>{item.entry}</td>
+            <td>{item.IPA}</td>
+            <ClickableCell>
+              {item.example_chinese}{' '}
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: numToSuperScript(item.example_romanisation),
+                }}
+              />{' '}
+              ({item.example_english})
+            </ClickableCell>
+            <td>{item.comments}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+const ClickableCell = styled.td`
+  cursor: pointer;
+`
