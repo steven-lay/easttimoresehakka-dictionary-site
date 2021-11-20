4f4d43b8e58028f6975fe8ce578e561e89fcb6cf
Steven Lay <arbiter1000@gmail.com>
2021-11-20 11:29:25 +1100

    Move PhonoTable body content outside table

--- src/components/PhonoTable.jsx
+++ src/components/PhonoTable.jsx
@@ -4,6 +4,18 @@ import styled from 'styled-components'
 import ToSuper from './ToSuper'
 
 export default function PhonoTable({ data, colTitles }) {
+  const tableBodyContent = data.map(item => (
+    <tr key={item.entry}>
+      <td>{item.entry}</td>
+      <td>{item.IPA}</td>
+      <ClickableCell>
+        {item.example_chinese} <ToSuper inputTxt={item.example_romanisation} />{' '}
+        ({item.example_english})
+      </ClickableCell>
+      <td>{item.comments}</td>
+    </tr>
+  ))
+
   return (
     <TableWrapper>
       <table>
@@ -14,20 +26,7 @@ export default function PhonoTable({ data, colTitles }) {
             ))}
           </tr>
         </thead>
-        <tbody>
-          {data.map(item => (
-            <tr key={item.entry}>
-              <td>{item.entry}</td>
-              <td>{item.IPA}</td>
-              <ClickableCell>
-                {item.example_chinese}{' '}
-                <ToSuper inputTxt={item.example_romanisation} /> (
-                {item.example_english})
-              </ClickableCell>
-              <td>{item.comments}</td>
-            </tr>
-          ))}
-        </tbody>
+        <tbody>{tableBodyContent}</tbody>
       </table>
     </TableWrapper>
   )
