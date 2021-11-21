0b09e9ba9efeb77bf716baab1a1069250011c42c
Steven Lay <arbiter1000@gmail.com>
2021-11-21 23:47:06 +1100

    Add responsive viewport meta

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -21,6 +21,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>East Timorese Hakka</title>
       </Head>
       <GlobalStyles />
