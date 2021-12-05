6d3543e67bcc62e042795900f25675c2f63570e2
steven-lay <arbiter1000@gmail.com>
2021-12-05 23:57:12 +1100

    Remove numToSuper function
    No longer needed

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,8 +1,5 @@
+// eslint-disable-next-line import/prefer-default-export
 export function playAudio(path) {
   const audio = new Audio(path)
   audio.play()
-}
-
-export function numToSuper(inputStr) {
-  return inputStr.replace(/([0-9])/g, '<sup>$&</sup>')
 }
