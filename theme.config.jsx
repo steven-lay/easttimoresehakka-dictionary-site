b1e73ddfd3b86e1166be4f26403e7a52f81e7cf6
Steven Lay <arbiter1000@gmail.com>
2022-12-07 00:08:31 +1100

    Initial commit

---
+++ theme.config.jsx
@@ -0,0 +1,13 @@
+export default {
+  logo: <span>East Timorese Hakka</span>,
+  project: {
+    link: 'https://github.com/steven-lay/easttimoresehakka-dictionary-site/',
+  },
+  docsRepositoryBase: 'https://github.com/steven-lay/easttimoresehakka-dictionary-site/',
+  feedback: {
+    content: undefined
+  },
+  editLink: {
+    component: undefined
+  },
+}
