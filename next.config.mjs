f169bac6f2b896f502f4623d91ef29b8926fb1a5
Steven Lay <arbiter1000@gmail.com>
2022-12-18 15:04:38 +1100

    Use remark-supersub plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
-const withNextra = require('nextra')({
+import nextra from 'nextra'
+import remarkSuperSub from 'remark-supersub';
+
+const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
+  mdxOptions: {
+    remarkPlugins: [
+      remarkSuperSub
+    ]
+  },
   staticImage: true
 })
 
-module.exports = withNextra()
+export default withNextra()
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
